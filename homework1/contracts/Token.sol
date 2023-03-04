a86d69294879caedb5c4bb4959f45f8a3d75209e
Antonio Alfonso <antonioalfonso.work@gmail.com>
2023-03-04 11:13:00 +0100

    feat: first token contract

---
+++ homework1/contracts/Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+  constructor(
+    string memory name,
+    string memory symbol,
+    uint256 supply
+  ) ERC20(name, symbol) {
+      _mint(msg.sender, supply * (1e18));
+  }
+}
